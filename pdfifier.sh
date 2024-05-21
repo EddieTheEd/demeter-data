40c0de77bacdd8790823544741f89b05678b32c9
EddieTheEd <edwardleehuynh@gmail.com>
2024-05-21 17:43:08 +0800

    data/chemistry/y12/sem1 exams

--- pdfifier.sh
+++ pdfifier.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-
-convert_docx_to_pdf() {
+convert_to_pdf() {
     input_file="$1"
-    output_file="${input_file%.docx}.pdf"
+    output_file="${input_file%.*}.pdf"
     
     if [ ! -f "$output_file" ]; then
         echo "Converting $input_file to PDF..."
         if [ "$2" != "test" ]; then
-            doc2pdf "$input_file"
+            if [[ "${input_file##*.}" =~ ^(docx|doc|DOCX|DOC|docm)$ ]]; then
+                doc2pdf "$input_file"
+            fi
         fi
     else
         echo "PDF already exists for $input_file"
@@ -18,8 +18,8 @@ traverse_directories() {
     for file in "$1"/*; do
         if [ -d "$file" ]; then
             traverse_directories "$file" "$2"
-        elif [ "${file##*.}" = "docx" ] || [ "${file##*.}" = "doc" ] || [ "${file##*.}" = "DOC" ] || [ "${file##*.}" = "DOCX" ] || [ "${file##*.}" = "docm" ]; then
-            convert_docx_to_pdf "$file" "$2"
+        elif [[ "${file##*.}" =~ ^(docx|doc|DOCX|DOC|docm)$ ]]; then
+            convert_to_pdf "$file" "$2"
         fi
     done
 }
@@ -33,3 +33,4 @@ main() {
 }
 
 main "$@"
+
